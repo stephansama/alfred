9968d603d4dc9826165ba1e0632e3d56874cc665
Stephan Randle <stephanrandle@hotmail.com>
2025-01-06 01:26:21 -0500

    feat: created collection script

---
+++ collect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+current_path=$(pwd)
+
+alfred_path="$HOME/Library/Application Support/Alfred/Alfred.alfredpreferences"
+
+cd "$alfred_path" || exit
+
+zip -r workflows.zip "workflows"
+
+mv workflows.zip "$current_path/workflows.zip"
